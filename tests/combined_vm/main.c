03edc7d1790e852a40a5adc3becc7991d1c1a49b
Roy Ward <roy@trurl.niesward.home>
2026-08-26 18:11:56 +1200

    got rid of obsolete refs stuff

---
+++ tests/combined_vm/main.c
@@ -0,0 +1,26 @@
+#include "combined_vm.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+    Program p;
+    program_load(&p,"combined_vm.rwo");
+    program_disassemble(&p,stdout);
+    RWInstance* exe=(RWInstance*)malloc(sizeof(RWInstance));
+    rw_instance_init(exe,&p);
+
+    int64_t result0;
+    int64_t result1;
+    bool resultb;
+    int ret_code;
+
+    printf("f:");
+    ret_code=rw_f(exe,&result0);
+    if(!ret_code && result0==3) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    program_unload(&p);
+    rw_instance_unload(exe);
+    return EXIT_SUCCESS;
+}
