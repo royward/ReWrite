e0bb3b3144a8b3c2566fc1b0df9057a1523c2585
Roy Ward <roy@trurl.niesward.home>
2026-09-21 22:27:36 +1200

    argret removal refactor finished

--- tests/combined/tests.h
+++ tests/combined/tests.h
@@ -82,10 +82,10 @@
     ret_code=rw_fact2(exe,10,&result0);
     if(!ret_code && result0==3628800) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
 
-    // printf("fact_tail:");
-    // ret_code=rw_fact_tail(exe,10,&result0);
-    // if(!ret_code && result0==3628800) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
+    printf("fact_tail:");
+    ret_code=rw_fact_tail(exe,10,&result0);
+    if(!ret_code && result0==3628800) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
     printf("fib:");
     ret_code=rw_fib(exe,10,&result0);
     if(!ret_code && result0==89) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
@@ -98,80 +98,80 @@
     ret_code=rw_multiret(exe,10,6,&result0);
     if(!ret_code && result0==-3) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
 
-    // printf("neg:");
-    // ret_code=rw_neg(exe,true,&resultb);
-    // if(!ret_code && !resultb) { printf("success\n"); } else { printf("fail:%d %d\n",ret_code,(int)resultb); };
-    //
-    // printf("neg:");
-    // ret_code=rw_neg(exe,false,&resultb);
-    // if(!ret_code && resultb) { printf("success\n"); } else { printf("fail:%d %d\n",ret_code,(int)resultb); };
-    //
-    // printf("process:");
-    // ret_code=rw_process(exe,4,&result0);
-    // if(!ret_code && result0==8) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
-    // printf("process:");
-    // ret_code=rw_process(exe,-4,&result0);
-    // if(!ret_code) {
-    //     printf("fail\n");
-    // } else {
-    //     const char* function;
-    //     uint32_t line;
-    //     rw_instance_get_error(exe,&line,&function);
-    //     if(ret_code==10 && strcmp(function,"process")==0) {
-    //         printf("success line:%d\n",line);
-    //     } else {
-    //         printf("fail:%d %s\n",ret_code,function);
-    //     }
-    // };
-    //
-    // printf("use_twice:");
-    // ret_code=rw_use_twice(exe,10,&result0);
-    // if(!ret_code && result0==900) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
-    // printf("break_rhs:");
-    // ret_code=rw_break_rhs(exe,4,&result0);
-    // if(!ret_code && result0==12) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
-    // printf("break_rhs:");
-    // ret_code=rw_break_rhs(exe,-4,&result0);
-    // if(!ret_code) {
-    //     printf("fail\n");
-    // } else {
-    //     const char* function;
-    //     uint32_t line;
-    //     rw_instance_get_error(exe,&line,&function);
-    //     if(ret_code==4 && strcmp(function,"break_rhs")==0) {
-    //         printf("success line:%d\n",line);
-    //     } else {
-    //         printf("fail:%d %s\n",ret_code,function);
-    //     }
-    // };
-    //
-    // printf("break_rhs2:");
-    // ret_code=rw_break_rhs2(exe,4,4,&result0);
-    // if(!ret_code && result0==12) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
-    // printf("break_rhs2:");
-    // ret_code=rw_break_rhs2(exe,4,-4,&result0);
-    // if(!ret_code) {
-    //     printf("fail\n");
-    // } else {
-    //     const char* function;
-    //     uint32_t line;
-    //     rw_instance_get_error(exe,&line,&function);
-    //     if(ret_code==4 && strcmp(function,"break_rhs2")==0) {
-    //         printf("success line:%d\n",line);
-    //     } else {
-    //         printf("fail:%d %s\n",ret_code,function);
-    //     }
-    // };
-    //
-    // printf("noparam:");
-    // ret_code=rw_noparam(exe,&result0);
-    // if(!ret_code && result0==3) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
-    // printf("to_uppercase:");
-    // ret_code=rw_to_uppercase(exe,'x',&resultc);
-    // if(!ret_code && resultc=='X') { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
-    //
+    printf("neg:");
+    ret_code=rw_neg(exe,true,&resultb);
+    if(!ret_code && !resultb) { printf("success\n"); } else { printf("fail:%d %d\n",ret_code,(int)resultb); };
+
+    printf("neg:");
+    ret_code=rw_neg(exe,false,&resultb);
+    if(!ret_code && resultb) { printf("success\n"); } else { printf("fail:%d %d\n",ret_code,(int)resultb); };
+
+    printf("process:");
+    ret_code=rw_process(exe,4,&result0);
+    if(!ret_code && result0==8) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    printf("process:");
+    ret_code=rw_process(exe,-4,&result0);
+    if(!ret_code) {
+        printf("fail\n");
+    } else {
+        const char* function;
+        uint32_t line;
+        rw_instance_get_error(exe,&line,&function);
+        if(ret_code==10 && strcmp(function,"process")==0) {
+            printf("success line:%d\n",line);
+        } else {
+            printf("fail:%d %s\n",ret_code,function);
+        }
+    };
+
+    printf("use_twice:");
+    ret_code=rw_use_twice(exe,10,&result0);
+    if(!ret_code && result0==900) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    printf("break_rhs:");
+    ret_code=rw_break_rhs(exe,4,&result0);
+    if(!ret_code && result0==12) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    printf("break_rhs:");
+    ret_code=rw_break_rhs(exe,-4,&result0);
+    if(!ret_code) {
+        printf("fail\n");
+    } else {
+        const char* function;
+        uint32_t line;
+        rw_instance_get_error(exe,&line,&function);
+        if(ret_code==4 && strcmp(function,"break_rhs")==0) {
+            printf("success line:%d\n",line);
+        } else {
+            printf("fail:%d %s\n",ret_code,function);
+        }
+    };
+
+    printf("break_rhs2:");
+    ret_code=rw_break_rhs2(exe,4,4,&result0);
+    if(!ret_code && result0==12) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    printf("break_rhs2:");
+    ret_code=rw_break_rhs2(exe,4,-4,&result0);
+    if(!ret_code) {
+        printf("fail\n");
+    } else {
+        const char* function;
+        uint32_t line;
+        rw_instance_get_error(exe,&line,&function);
+        if(ret_code==4 && strcmp(function,"break_rhs2")==0) {
+            printf("success line:%d\n",line);
+        } else {
+            printf("fail:%d %s\n",ret_code,function);
+        }
+    };
+
+    printf("noparam:");
+    ret_code=rw_noparam(exe,&result0);
+    if(!ret_code && result0==3) { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
+    printf("to_uppercase:");
+    ret_code=rw_to_uppercase(exe,'x',&resultc);
+    if(!ret_code && resultc=='X') { printf("success\n"); } else { printf("fail:%d %ld\n",ret_code,result0); };
+
